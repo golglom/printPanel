b84b421297c3af4305802f6390777df1ea536bb8
georges-golden <zicombusiness74@gmail.com>
2025-05-24 10:39:11 +0000

    proxy deleting

--- gestionImprimerie/vite.config.js
+++ gestionImprimerie/vite.config.js
@@ -5,13 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5174,
-    proxy: {
-      '/api': {
-        target: 'https://printpanel.onrender.com',
-        changeOrigin: true,
-      },
-    },
+    port: 5174
   },
   build:{
     outDir: 'dist'
