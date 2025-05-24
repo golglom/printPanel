267fcb056cf62dec1e63a090ca1c062c496e7b7e
georges-golden <zicombusiness74@gmail.com>
2025-05-24 11:34:47 +0000

    cors update

--- backend/server.js
+++ backend/server.js
@@ -14,12 +14,7 @@ connectDB();
 const app = express();
 
 
-app.use(cors({
-
-  origin: 'https://printipro.vercel.app/' ,
-  credentials: true
-}
-));
+app.use(cors());
 
 app.use(express.json());
 
