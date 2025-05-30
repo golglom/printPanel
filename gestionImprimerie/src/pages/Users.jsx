7390df09dde61befd74b8e7dec9e5fc51538d674
georges-golden <156268532+golglom@users.noreply.github.com>
2025-05-30 19:47:25 +0000

    admin role button

--- gestionImprimerie/src/pages/Users.jsx
+++ gestionImprimerie/src/pages/Users.jsx
@@ -161,7 +161,7 @@ function Users() {
               </td>
               <td>{user.lastLogin ? new Date(user.lastLogin).toLocaleDateString() : 'Jamais'}</td>
               <td>
-              {/* {userRole === 'admin' && ( */}
+                {userRole === 'admin' && ( 
                     <>
                       <button onClick={() => handleEdit(user)} className="btn btn-warning btn-sm me-2">
                         <i className="bi bi-pencil"></i>
@@ -170,7 +170,7 @@ function Users() {
                         <i className="bi bi-trash"></i>
                       </button>
                     </>
-                  {/* )} */}
+                )}
               </td>
             </tr>
           ))}
