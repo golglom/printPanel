829e667646fbe51e58986fea650cbbed80561354
georges-golden <zicombusiness74@gmail.com>
2025-06-02 14:16:07 +0000

    error message changed

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -12,12 +12,12 @@ export const login = async (req, res) => {
 
     const user = await User.findOne({ username });
     if (!user) {
-      return res.status(401).json({ message: 'Invalid credentials' });
+      return res.status(401).json({ message: 'Invalid username' });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(401).json({ message: 'Invalid credentials' });
+      return res.status(401).json({ message: 'Invalid password' });
     }
 
     const token = jwt.sign(
